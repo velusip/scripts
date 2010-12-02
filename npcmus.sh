f6f8eaa5132569bd3a542bc56ddf9e4c7e0d483e
velusip <velusip@gmail.com>
2010-12-01 23:01:38 -0800

    modified:   npcmus.sh added error checking added infor for adding to xmobar

--- npcmus.sh
+++ npcmus.sh
@@ -15,7 +15,9 @@
 #     /alias np exec -nosh -window - /path/to/npcmus.sh
 #   * nicotine - print to channel with /np
 #     Alt + e, n --> Other --> enter /path/to/npcmus.sh
+#   * xmobarrc - commands = []
+#     Run Com "/path/to/npcmus.sh" "" "npcmus" 5
 #
 #########################################################################
 
-cmus-remote -Q | awk 'function toEnd(iStr, skip) {return substr(iStr, match(iStr,skip) + length(skip) + 1);} $2 ~ "artist" {artist = toEnd($0,"artist");} $2 ~ "title" {title = toEnd($0,"title");} END { print "Now playing: ", artist, " - ", title; }'
+cmus-remote -Q | awk 'function toEnd(iStr, skip) { return substr(iStr, match(iStr, skip) + length(skip) + 1); } NR == 1 {if($0 ~ "running") {offline = 1; exit 1;} } NR == 2 && $1 ~ "file" {file = toEnd($0, "file");} $2 ~ "artist" {artist = toEnd($0, "artist");} $2 ~ "title" {title = toEnd($0, "title");} END { if(artist && title) {print "Now playing: ", artist, " - ", title;} else if(file) {print "Now playing: ", file;} }'
